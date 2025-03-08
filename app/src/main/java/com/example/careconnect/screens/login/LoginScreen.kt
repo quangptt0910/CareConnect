aff93f678bf02b75a115b4494e2dcc866aabc555
nerikeb <283010@student.pwr.edu.pl>
2025-03-08 21:43:08 +0100

    Added theme and first screen layout

---
+++ app/src/main/java/com/example/careconnect/screens/login/LoginScreen.kt
@@ -0,0 +1,33 @@
+package com.example.careconnect.screens.login
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(
+
+){
+
+}
+
+@Composable
+fun LoginScreenContent(
+
+){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ){
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(
+
+){
+    LoginScreenContent()
+}
